57ea10a05c3b7b07e413c5017951ea8f8aeb296d
ayc726 <AYC726@gmail.com>
2013-11-12 20:12:24 -0500

    updated new talents for amoeba

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+amoebas = Amoeba.create([{name: "jim", generation: 1}, {name: "joe", generation: 1}, {name: "bill", generation: 1}])
+
+amoeba1 = Amoeba.create(name: "jim", generation: 1)
+amoeba1.build_talent
+amoeba1.talent.name = "blackflips"
+amoeba1.talent.save
+
+amoeba2 = Amoeba.create(name: "joe", generation: 1)
+amoeba2.build_talent
+amoeba2.talent.name = "contorionist"
+amoeba2.talent.save
+
+act1 = Act.create(name: "the greatest show on earth", date: "12/12/2014")
+act2 = Act.create(name: "the second best show on earth", date: "12/12/2014")
+
+join_table1 = AmoebaAct.create(amoeba: amoeba1, act: act1)
+join_table2 = AmoebaAct.create(amoeba: amoeba2, act: act1)
